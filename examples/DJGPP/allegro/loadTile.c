92169c6081ba6b509e68128167121317c1cf9559
PompPenguin <pomppenguin@gmail.com>
2021-01-06 11:14:16 -0500

    updated DJGPP (MS-DOS) loadTile example
    --updated loadTile.c
      --added transparent sprites
      --added sprite flipping
    --updated loadTile.exe
      --built from new loadTile.c
    --updated SL.bmp
      --corrected bitmap color index

--- examples/DJGPP/allegro/loadTile.c
+++ examples/DJGPP/allegro/loadTile.c
@@ -21,12 +21,35 @@
  
 
 void DrawTiles(struct layerInstances *layer,BITMAP *texture,BITMAP *buffer,int offset){
+
+    BITMAP *bmp = NULL; //tmp used to hold flipped tiles
+    bmp = create_bitmap(offset,offset);
+    clear_bitmap(bmp);
+
 	for(int y=layer->autoTiles_data_ptr->count ;y-- > 0;){ 
-            
-        blit(texture, buffer, layer->autoTiles_data_ptr[y].SRCx, layer->autoTiles_data_ptr[y].SRCy, layer->autoTiles_data_ptr[y].x,
-		     layer->autoTiles_data_ptr[y].y, offset, offset);
-		
+        
+        masked_blit(texture, bmp, layer->autoTiles_data_ptr[y].SRCx, layer->autoTiles_data_ptr[y].SRCy, 0,
+		     0, offset, offset);
+        
+        switch(layer->autoTiles_data_ptr[y].f){
+            case 0 :
+                draw_sprite(buffer,bmp,layer->autoTiles_data_ptr[y].x,layer->autoTiles_data_ptr[y].y);
+                clear_bitmap(bmp);
+            //flip horizontal
+            case 1 :
+                draw_sprite_h_flip(buffer,bmp,layer->autoTiles_data_ptr[y].x,layer->autoTiles_data_ptr[y].y);
+                clear_bitmap(bmp);
+            //flip vertical
+            case 2 :
+                draw_sprite_v_flip(buffer,bmp,layer->autoTiles_data_ptr[y].x,layer->autoTiles_data_ptr[y].y);
+                clear_bitmap(bmp);
+            //flip both
+            case 3 :
+               draw_sprite_vh_flip(buffer,bmp,layer->autoTiles_data_ptr[y].x,layer->autoTiles_data_ptr[y].y);
+               clear_bitmap(bmp); 
+        }  	
     }
+    destroy_bitmap(bmp);
 }
 END_OF_FUNCTION(DrawTiles);
 
@@ -34,16 +57,18 @@ int main(int argc, char *argv[])
 {
     allegro_init();
     install_keyboard();
-	//can increase if video card has above 512k of VRAM
+
     set_color_depth(8);
 
-    set_gfx_mode(GFX_AUTODETECT, 640, 480, 0, 0);
+    set_gfx_mode(GFX_AUTODETECT, 320, 240, 0, 0);
 
     BITMAP *my_pic = NULL;
-    my_pic = load_bitmap("sl.bmp",NULL);
+    PALETTE the_palette;
+    my_pic = load_bitmap("sl.bmp",the_palette);
+    
 	
 	BITMAP *buffer = NULL;
-    buffer = create_bitmap(640,480);
+    buffer = create_bitmap(320,240);
 	
 	clear_keybuf();
 
@@ -67,17 +92,15 @@ int main(int argc, char *argv[])
     struct layerInstances *lvl_one_background;
     lvl_one_background = getLayer("Bg_textures",lvl_one->uid);
 	
+    set_palette(the_palette);
 	//load background tiles
 	DrawTiles(lvl_one_background,my_pic,buffer,16);
 	//load foreground tiles
 	DrawTiles(lvl_one_collisions,my_pic,buffer,16);
 	
 	//load from buffer to screen
-	blit(buffer, screen,0,0,0,0,640,480);
-	
-
-
-    
+    masked_blit(buffer, screen,0,0,0,0,320,240);
+        
     readkey();
 
     destroy_bitmap(my_pic);
